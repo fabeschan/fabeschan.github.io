684a88a65886f14db6581a0e5e98f84c00a2ee4f
Fabian Chan <fabeschan@gmail.com>
2014-07-08 15:39:14 +0800

    add projects

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 
 all:
 	php index.php > index.html
+
+v: all
+	open index.html
--- data.php
+++ data.php
@@ -5,13 +5,35 @@ $user = array(
   'username' => 'fabeschan', # github username
   'name' => 'Fabian Chan', # real name
   'title' => 'Software Developer', # job title
-  'description' => '<p>This page is generated to show an example. Page data is generated from data.php</p>', # about you
+  'description' => '<p>Generic Description! Here are some projects as a result of my hobbies.</p>', # about you
   'postscript' => '<h2>How about this?</h2><p>To make your own one, fork this project and rename the repository &lt;yourname&gt;.github.com</p>', # postscript after projects
 );
 
 $projects = array(
   # <projectname> => data array
-  'github-profile' => array(
+  'GenomeSequences' => array(
+    'description' => 'Maximal Matches in Genome Sequences',
+    'longdescription' => 'Locate matches between two string representations of genome sequences of minimal edit distance. Maximal Matches in O(n) Time. Edit Distance Aligner in O(n) Space. (ECE358 Project)',
+    'github' => 'fabeschan/genomesequences',
+    'submodule' => False, # has submodule. default is False
+    'cocoapod' => False, # has cocoapod. default is False
+    'minor' => False, # is minor project. default is False
+    'open' => array('type' => 'edit_n_make', 'file' => 'data.php'), # project opener
+  ),
+
+  # <projectname> => data array
+  'StringToWav' => array(
+    'description' => 'String to wav music converter',
+    'longdescription' => 'A fun project to turn a string into music. Supports polymelodic compositions',
+    'github' => 'fabeschan/stringtowav',
+    'submodule' => False, # has submodule. default is False
+    'cocoapod' => False, # has cocoapod. default is False
+    'minor' => False, # is minor project. default is False
+    'open' => array('type' => 'edit_n_make', 'file' => 'data.php'), # project opener
+  ),
+
+  # <projectname> => data array
+  'Github-Profile' => array(
     'description' => 'Github profile generator based on manual input data.', # project description
     'github' => 'fabeschan/github-profile', # github identifier
     'submodule' => False, # has submodule. default is False
